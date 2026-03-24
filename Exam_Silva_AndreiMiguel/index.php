384a4c93c816ea233f55e1f41668905b04167819
D-Drei <0.7.andreisilvaa@gmail.com>
2026-03-24 21:53:21 +0800

    index.php

--- Exam_Silva_AndreiMiguel/index.php
+++ Exam_Silva_AndreiMiguel/index.php
@@ -7,7 +7,7 @@ session_start();
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>INDEX PAGE</title>
+  <title>Index</title>
   <link rel="stylesheet" href="style.css" />
   <style>
     .video-bg-wrapper {
